e5eb85708573da54760dbca3540b7f819b326593
salemaziel <mymainemail0501@gmail.com>
2023-08-09 21:35:42 -0700

    testingghifufu

--- src/components/PageComponents/Home/s3MenuButton.js
+++ src/components/PageComponents/Home/s3MenuButton.js
@@ -11,8 +11,9 @@ const S3MenuButton = () => {
         		<div className="flex justify-center flex-initial">
 
 							<span className="w-full py-5 text-xl text-center text-white bg-red-600 rounded-md cursor-pointer fontFredoka hover:bg-red-800" 
-data-glf-cuid="8e7c87fd-2ed0-44b4-9886-8626e340905e"
-data-glf-ruid="d3afdc87-1713-43aa-bd55-854fdf812f01"     id="glfButton1"> See Menu &amp; Order 
+data-glf-cuid="fcc9622a-6d23-4af1-895b-5ff1021ff7b0" 
+data-glf-ruid="83c6af9e-70c9-4dc6-b2e8-223c37d5f648"
+id="glfButton1"> See Menu &amp; Order 
                             </span>
                             
                 </div>
