53c89262f18ff5252c004001d34146f70535c5af
salemaziel <mymainemail0501@gmail.com>
2024-07-02 20:52:56 -0700

    Refactor CSS styles for pillz component

---
+++ src/components/pillz.js
@@ -0,0 +1,149 @@
+import React from "react"
+import "./pillz.css"
+
+import Fade from "react-reveal/Fade"
+
+const Pillz = () => {
+  return (
+    <>
+      <Fade top>
+        <div className="container">
+          <a href="https://youtube.com"  className="card-left">
+            <div className="card-image">
+              <img  src="https://images.pexels.com/photos/220072/pexels-photo-220072.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </a>
+          <div className="card-top">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/1011334/pexels-photo-1011334.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Quisque cursus, metus vitae pharetra auctor.</p>
+            </div>
+          </div>
+          <div className="card-right">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/35828/soap-bubble-colorful-ball-soapy-water.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Ut eu diam at pede suscipit sodales.</p>
+            </div>
+          </div>
+          <div className="card-right">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/701855/pexels-photo-701855.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>
+                Donec lacus nunc, viverra nec, blandit vel, egestas et, augue.
+              </p>
+            </div>
+          </div>
+          <div className="card-top">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/668295/pexels-photo-668295.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div className="card-bottom">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/63238/pexels-photo-63238.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Ut eu diam at pede suscipit sodales.</p>
+            </div>
+          </div>
+          <div className="card-left">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/997725/pexels-photo-997725.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div className="card-bottom">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/585581/pexels-photo-585581.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Quisque cursus, metus vitae pharetra auctor.</p>
+            </div>
+          </div>
+          <div className="card-right">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/532561/pexels-photo-532561.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div className="card-top">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/279376/pexels-photo-279376.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Integer lacinia sollicitudin massa. Cras metus.</p>
+            </div>
+          </div>
+          <div className="card-left">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/701855/pexels-photo-701855.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div className="card-left">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/262577/pexels-photo-262577.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Ut eu diam at pede suscipit sodales.</p>
+            </div>
+          </div>
+          <div className="card-top">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/978342/pexels-photo-978342.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div className="card-bottom">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/305831/pexels-photo-305831.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Quisque cursus, metus vitae pharetra auctor.</p>
+            </div>
+          </div>
+          <div className="card-top">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/355728/pexels-photo-355728.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>
+                Donec lacus nunc, viverra nec, blandit vel, egestas et, augue.
+              </p>
+            </div>
+          </div>
+          <div className="card-right">
+            <div className="card-image">
+              <img src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+            </div>
+            <div className="card-text">
+              <p>Ut eu diam at pede suscipit sodales.</p>
+            </div>
+          </div>
+        </div>
+      </Fade>
+    </>
+  )
+}
+
+export default Pillz
